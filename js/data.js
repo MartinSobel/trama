631ea3a674eb80506d93af0c7631a31158d604bb
MartinSobel <marto.sobel@gmail.com>
2020-12-13 00:05:43 -0300

    orden de data

--- js/data.js
+++ js/data.js
@@ -2,7 +2,6 @@ let descriptions = [
     [
         'La tierra, un gran silencio',
         '1h 20min',
-        "No se veían hace más de un año Tendrán que hablar y tendrán que hacer silencio El reencuentro tiene un rostro ambiguo",
-        
+        "No se veían hace más de un año Tendrán que hablar y tendrán que hacer silencio. El reencuentro tiene un rostro ambiguo...",
     ]
 ]
--- js/main.js
+++ js/main.js
@@ -1,25 +1,28 @@
 function renderDescription(){
-    var title = document.createElement('h3');
+    // Borrar el div primero
+    let randomNum = console.log(Math.floor((Math.random() * 10) + 1));
+
+    let title = document.createElement('h3');
     title.innerText = descriptions[0][0] + ' ';
     document.getElementById("description").appendChild(title);
 
-    var duracion = document.createElement('span');
+    let duracion = document.createElement('span');
     duracion.innerText = descriptions[0][1] + ' ';
     document.getElementById("description").appendChild(duracion);
 
-    var timeIcon = document.createElement('i');
+    let timeIcon = document.createElement('i');
     timeIcon.classList.add('icofont-ui-clock');
     document.getElementById("description").appendChild(timeIcon);
 
-    var description = document.createElement('p');
+    let description = document.createElement('p');
     description.innerText = descriptions[0][2];
     document.getElementById("description").appendChild(description);
 
-    var trailer = document.createElement('a');
+    let trailer = document.createElement('a');
     trailer.innerText = "Ver trailer";
     document.getElementById("description").appendChild(trailer);
 
-    var ver = document.createElement('a');
+    let ver = document.createElement('a');
     ver.innerText = "Ver ahora";
     ver.id = 'ver';
     document.getElementById("description").appendChild(ver);
