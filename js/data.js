19f9dcb586089046ae73f708c80e4a79e71d9740
MartinSobel <marto.sobel@gmail.com>
2020-12-13 00:44:04 -0300

    funcionamiento con varias descripciones

--- js/data.js
+++ js/data.js
@@ -1,7 +1,53 @@
-let descriptions = [
+let descriptionsES = [
+    [],
     [
         'La tierra, un gran silencio',
         '1h 20min',
         "No se veían hace más de un año Tendrán que hablar y tendrán que hacer silencio. El reencuentro tiene un rostro ambiguo...",
-    ]
+    ],
+    [
+        'Otro titulo 1',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 2',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 3',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 4',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 5',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 6',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 7',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 8',
+        '14min',
+        "Descripcion pendiente...",
+    ],
+    [
+        'Otro titulo 9',
+        '14min',
+        "Descripcion pendiente...",
+    ],
 ]
--- js/main.js
+++ js/main.js
@@ -1,13 +1,23 @@
 function renderDescription(){
-    // Borrar el div primero
-    let randomNum = console.log(Math.floor((Math.random() * 10) + 1));
+    // Borrar el div
+    if(document.getElementById("description")){
+        document.body.removeChild(document.getElementById("description"));
+    }
+
+    // Genera el div
+    let div = document.createElement('div');
+    document.body.appendChild(div);
+    div.id = "description";
+
+    let randomNum = Math.floor((Math.random() * 10) + 1);
 
     let title = document.createElement('h3');
-    title.innerText = descriptions[0][0] + ' ';
+    title.id = 'title';
+    title.innerText = descriptionsES[randomNum][0] + ' ';
     document.getElementById("description").appendChild(title);
 
     let duracion = document.createElement('span');
-    duracion.innerText = descriptions[0][1] + ' ';
+    duracion.innerText = descriptionsES[randomNum][1] + ' ';
     document.getElementById("description").appendChild(duracion);
 
     let timeIcon = document.createElement('i');
@@ -15,7 +25,7 @@ function renderDescription(){
     document.getElementById("description").appendChild(timeIcon);
 
     let description = document.createElement('p');
-    description.innerText = descriptions[0][2];
+    description.innerText = descriptionsES[randomNum][2];
     document.getElementById("description").appendChild(description);
 
     let trailer = document.createElement('a');
@@ -26,4 +36,8 @@ function renderDescription(){
     ver.innerText = "Ver ahora";
     ver.id = 'ver';
     document.getElementById("description").appendChild(ver);
+}
+
+function renderEpisode(randomNum){
+    
 }
