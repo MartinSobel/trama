a588c1e837fb3a2f50c0a81dd6b053dfc9717ce1
MartinSobel <marto.sobel@gmail.com>
2020-12-13 23:30:11 -0300

    contenido placeholder

--- js/data.js
+++ js/data.js
@@ -7,57 +7,57 @@ let descriptionsES = [
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
     ],
     [
-        'Otro titulo 1',
-        '14min',
-        "Descripcion pendiente...",
+        'Titulazo',
+        '54min',
+        "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, laborum.",
+        "https://comunidad.iebschool.com/iebs/files/2016/02/Taco_Party_4.gif"
+    ],
+    [
+        'Episodio',
+        '1h 10min',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto",
+        "https://blog.commlabindia.com/wp-content/uploads/2019/07/animated-gifs-corporate-training.gif"
+    ],
+    [
+        'Alto episodio',
+        '36min',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
     ],
     [
-        'Otro titulo 2',
-        '14min',
-        "Descripcion pendiente...",
+        'Las aventuras de Dama',
+        '1h 8min',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
     ],
     [
-        'Otro titulo 3',
-        '14min',
-        "Descripcion pendiente...",
+        'Travesias de Jada',
+        '1h 1m',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
     ],
     [
-        'Otro titulo 4',
-        '14min',
-        "Descripcion pendiente...",
+        'Nombre episodio',
+        '21min',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
+        "https://images2.minutemediacdn.com/image/upload/c_crop,h_674,w_1200,x_0,y_113/f_auto,q_auto,w_1100/v1554999553/shape/mentalfloss/504605-publicdomain.gif"
+    ],
+    [
+        'Test',
+        '1h 17min',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
     ],
     [
-        'Otro titulo 5',
-        '14min',
-        "Descripcion pendiente...",
-        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
+        'Otro titulo',
+        '1h 43min',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
+        "https://www.marketingdirecto.com/wp-content/uploads/2016/03/gifs-fb.gif"
     ],
     [
-        'Otro titulo 6',
-        '14min',
-        "Descripcion pendiente...",
-        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
-    ],
-    [
-        'Otro titulo 7',
-        '14min',
-        "Descripcion pendiente...",
-        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
-    ],
-    [
-        'Otro titulo 8',
-        '14min',
-        "Descripcion pendiente...",
-        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
-    ],
-    [
-        'Otro titulo 9',
-        '14min',
-        "Descripcion pendiente...",
-        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif"
+        'Titulin',
+        '57min',
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+        "https://www.marketingdirecto.com/wp-content/uploads/2016/03/gifs-fb.gif"
     ],
 ]
