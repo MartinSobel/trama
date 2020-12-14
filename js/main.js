ec29e5caa746836ff0cf84629889055a8fd2d563
MartinSobel <marto.sobel@gmail.com>
2020-12-13 23:19:29 -0300

    ajustes esteticos info

--- js/main.js
+++ js/main.js
@@ -7,24 +7,28 @@ function renderDescription(){
     document.body.appendChild(div);
     div.id = "description";
 
+    let descriptionText = document.createElement('div');
+    div.appendChild(descriptionText);
+    descriptionText.id = "descriptionText";
+
     let randomNum = Math.floor((Math.random() * 10) + 1);
 
     let title = document.createElement('h3');
     title.id = 'title';
     title.innerText = descriptionsES[randomNum][0] + ' ';
-    document.getElementById("description").appendChild(title);
+    document.getElementById("descriptionText").appendChild(title);
 
     let duracion = document.createElement('span');
     duracion.innerText = descriptionsES[randomNum][1] + ' ';
-    document.getElementById("description").appendChild(duracion);
+    document.getElementById("descriptionText").appendChild(duracion);
 
     let timeIcon = document.createElement('i');
     timeIcon.classList.add('icofont-ui-clock');
-    document.getElementById("description").appendChild(timeIcon);
+    document.getElementById("descriptionText").appendChild(timeIcon);
 
     let description = document.createElement('p');
     description.innerText = descriptionsES[randomNum][2];
-    document.getElementById("description").appendChild(description);
+    document.getElementById("descriptionText").appendChild(description);
 
     let ver = document.createElement('i');
     ver.classList.add('icofont-play-alt-2');
@@ -45,26 +49,31 @@ function renderEpisode(randomNum){
     document.body.appendChild(div);
     div.id = "episode";
 
+    let info = document.createElement('div');
+    div.appendChild(info);
+    info.id = "info";
+
     let regresar = document.createElement('a');
     regresar.innerText = 'Regresar';
     regresar.id = 'regresar';
     regresar.addEventListener('click', function(){
         document.body.removeChild(document.getElementById("episode"));
     });
-    document.getElementById("episode").appendChild(regresar);
+    document.getElementById("info").appendChild(regresar);
 
     let title = document.createElement('h2');
     title.innerText = descriptionsES[randomNum][0];
     title.id = 'episodeTitle'
-    document.getElementById("episode").appendChild(title);
+    document.getElementById("info").appendChild(title);
 
     let description = document.createElement('p');
     description.innerText = descriptionsES[randomNum][2];
-    document.getElementById("episode").appendChild(description);
+    document.getElementById("info").appendChild(description);
 
     let video = document.createElement('video');
     video.width = '1000';
     video.controls = true;
+    video.controlsList = "nodownload";
     let source = document.createElement('source');
     source.src = 'https://www.w3schools.com/html/mov_bbb.mp4';
     video.appendChild(source);
