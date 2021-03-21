1e4f50bfee8ca378c4ba92ceee8abcfb13109a79
MartinSobel <marto.sobel@gmail.com>
2021-03-21 12:39:14 -0300

    update still

--- js/data.js
+++ js/data.js
@@ -4,67 +4,67 @@ let descriptionsES = [
         'La tierra, un gran silencio',
         '1h 20min',
         "No se veían hace más de un año Tendrán que hablar y tendrán que hacer silencio. El reencuentro tiene un rostro ambiguo...",
-        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
+        "/assets/images/thumbnails/La tierra.jpg",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'La paciencia de los fuertes',
+        'Em leaves the theater',
         '54min',
         "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam, laborum.",
         "https://comunidad.iebschool.com/iebs/files/2016/02/Taco_Party_4.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Episodio',
+        'The alley',
         '1h 10min',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto",
         "https://blog.commlabindia.com/wp-content/uploads/2019/07/animated-gifs-corporate-training.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Alto episodio',
+        'No te tiene que doler',
         '36min',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Las aventuras de Dama',
+        'Una incertidumbre que se propaga',
         '1h 8min',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Travesias de Jada',
+        'La nave de la distancia',
         '1h 1m',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Nombre episodio',
+        'Ana',
         '21min',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://images2.minutemediacdn.com/image/upload/c_crop,h_674,w_1200,x_0,y_113/f_auto,q_auto,w_1100/v1554999553/shape/mentalfloss/504605-publicdomain.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Test',
+        'Inercia',
         '1h 17min',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Otro titulo',
+        'Sirena en la ventana',
         '1h 43min',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus vel ut quae esse facere reiciendis nihil in architecto, fuga sequi nulla ad quaerat, ducimus voluptatum natus. Illo, consectetur. Enim, tempore.",
         "https://www.marketingdirecto.com/wp-content/uploads/2016/03/gifs-fb.gif",
         '<iframe src="https://player.vimeo.com/video/501090322" width="1200" height="360" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>'
     ],
     [
-        'Titulin',
+        '¿Qué quieren hacer con las cenizas?',
         '57min',
         "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         "https://www.marketingdirecto.com/wp-content/uploads/2016/03/gifs-fb.gif",
