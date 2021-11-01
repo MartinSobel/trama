5920af7976be3ea543311533fcb52b3e3fc50692
MartinSobel <marto.sobel@gmail.com>
2021-11-01 13:16:08 -0300

    funcionalidad en ingles

--- js/data.js
+++ js/data.js
@@ -65,3 +65,71 @@ let descriptionsES = [
         'Con: Sol Bordigoni, Jada Sirkin, Mumi Rujana, Rita Messina, Magalí Aldabe, Hernán Bustamante, Sharon Cohen Goldfrid, Jazmín Ruffo, Wem Wertheimer, Keni, Toni \n Dirección, producción, montaje: Dama David, Jada Sirkin Guión: Jada Sirkin, Dama David, Denisse Bustos, Sol Bordigoni, Rita Messina, Mumi Rujana \n Asistencia de arte: María Morín \n Cámara: Dama David, Jada Sirkin \n Sonido directo: Denisse Bustos, Camila Torres, Elsa Wirth, Dama David, Sharon Cohen Goldfrid, Wayra Inti, Facundo Martin, Julián Hercovich. \n Postproducción de sonido: Zub sonido - Emiliano Biaiñ, Marcos Zoppi, Facundo de Vedia \n Desgrabación: Magdalena Chattah \n Música: Eduardo Felenbok, Mumi Rujana, Jada Sirkin'
     ],
 ]
+
+let descriptionsEN = [
+    [],
+    [
+        'The Earth, a great silence',
+        '66 min',
+        "They haven't seen each other for more than a year they''ll have to talk and they''ll have to make silence Reunion has an ambiguous face",
+        "assets/images/thumbnails/La Tierra, un gran silencio 16-9 Dibujo.jpg",
+        '<iframe src="https://player.vimeo.com/video/631429742" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'With: Josefa Cavada, Tania Jofré, Munay Gelerstein Cavada, Verónica Salas, Marcela Urzay, Ana Iro, Sara Luna Aylwin, Anyela Elliesen, Vicente Jofré, Julieta Bernal, Lautaro Quiroz \n Direction, production and editing: Jada Sirkin, Dama David \n Script: Dama David, Jada Sirkin, Josefa Cavada, Tania Jofré \n Camera and color: Jada Sirkin \n Direct sound: Dama David, Jeremy Hatcher \n Sound post production: Zub sonido - Emiliano Biaiñ, Marcos Zoppi, Facundo de Vedia \n Opening and ending drawings: Tania Jofré \n Music: Julián Romero, Josefa Cavada, Nea Ducci, Ana Iro, Almeyda'
+    ],
+    [
+        'It shouldn’t hurt you',
+        '23 min',
+        "Tato and Clara had planned a sexy night. The conversation took them elsewhere. What if we start saying what we don't like?",
+        "assets/images/thumbnails/No te tiene que doler 16-9 Dibujo.jpg",
+        '<iframe src="https://player.vimeo.com/video/629299340?h=16d2f4cec7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'With: Lis Tejón, Edu Anderson, Mumi Rujana \n Direction, production, editing: Jada Sirkin, Dama David \n Script: Dama David, Jada Sirkin, Lis Tejón, Edu Anderson, Mumi Rujana \n Camera: Jada Sirkin, Magalí Aldabe \n Direct sound: José Ratti, Huber Cabello, Bayron Mendivelso \n Art: Magalí Aldabe \n Production assistance: Sharon Cohen Golrdfrid, Malena Morandini \n Still photo: Malena Morandini \n Lighting assistance: Toki Pfannkuche, Leonel Luciani \n Sound post production: Zub sonido - Emiliano Biaiñ, Marcos Zoopi, Facundo de Vedia \n Opening drawing and paintings: Cuno Sztark \n Translation: Adrián Gleizer \n Music: Mumi Rujana'
+    ],
+    [
+        'Em leaves the theater',
+        '55 min',
+        "Em sells the theater, her life project. What remains is rain, animals, pianos, friends, useless attempts to make sense.",
+        "assets/images/thumbnails/TRAMA Em Still 110 (corregida).jpg",
+        '<iframe src="https://player.vimeo.com/video/631418639" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'With: Mizu Desierto, Kelly Nesbitt, Stephanie Lanckton, Lolo Haha, Draven Chase, Kennedy, Ariel Bittner, Ira Echo, Karla Mi Lugo, Paula Helen \n Production: Jada Sirkin, Dama David, Mizu Desierto \n Direction, editing: Jada Sirkin, Dama David \n Script: Jada Sirkin, Dama David, Camila Torres, Mizu Desierto, Kelly Nesbitt, Stephanie Lanckton, Lolo Haha \n Camera and color: Jada Sirkin \n Direct sound: Dama David, Stephanie Lanckton, Kelly Nesbitt \n Sound post production: Zub sonido - Emiliano Biaiñ, Marcos Zoopi, Facundo de Vedia \n Production assistance: Kelly Nesbitt, Stephanie Lanckton \n Music: Mad Composer Lab, Carly Bates, Yaara Valey, Karla Mi Lugo, Stephanie Lanckton, Roland Toledo (a.k.a. Alter Structure), Ira Echo'
+    ],
+    [
+        'La nave de la distancia',
+        '36min',
+        "Lucas cancela el rodaje de su película y el sueño vuelve a quedar lejos, inaccesible. Aunque toca los árboles, asegura que no los siente. Por suerte no está solo.",
+        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
+        '<iframe src="https://player.vimeo.com/video/629299340?h=16d2f4cec7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'Con: Jada Sirkin, Magalí Aldabe, Manuel Fanego, Joel Roch, Rodrigo Soloeta, Kelly Nesbitt, Joaquín Astelarra Escobar, Mariana Vidal, Manuel Tiziano Kaplan, Wem Wertheimer, Hernán Bustamante, Melisa Piccioli, Franco Antonio de la Puente, Irunah Briozzo, Gael Higa Penna, Facundo Higa, Josefina Penna, Cecilia Jury, Mauro Vignetta, José Ratti, Bayron Mendivelso \n Dirección, producción, y montaje: Jada Sirkin, Dama David \n Guión: Jada Sirkin, Dama David, Manuel Fanego, Joel Roch, Magalí Aldabe, Rodrigo Soloeta, Wem Wertheimer, José Ratti, Kelly Nesbitt, Mariana Vidal, Manuel Tiziano Kaplan, Hernán Bustamante, Melisa Piccioli, Franco Antonio de la Puente, Irunah Briozzo, Gael Higa Penna, Facundo Higa, Josefina Penna, Cecilia Jury, Mauro Vignetta \n Cámara: Dama, Jada, Julián Hercovich, Dama David \n Color: Jada Sirkin \n Foto fija: Leonel Luciani, Toki Pfannkuche, Denisse Bustos Sonido directo: José Ratti, Bayron Mendivelso, Dama David, Denisse Bustos, Sol Bordigoni, Julián Hercovich, Sharon Cohen Goldfrid, Lola Haha \n Post producción de sonido: Zub sonido - Emiliano Biaiñ, Marcos Zoopi, Facundo de Vedia \n Desgrabación: Gualda Ruffo Música: Jada Sirkin, Irunah Briozzo, Rodrigo Soloeta, Cecilia Jury, Mauro Vignetta'
+    ],
+    [
+        'A spreading turbulence',
+        '46 min',
+        "October 2019. Max is moving to another country, but it's the others who are moving. Ana, Caro, Uli, Teo, Sabrina, Andi, Lili, Javiera, the Chilean people. Javiera has just arrived in Buenos Aires, she comes from participating in the revolt in Chile, she wants hugs and she wants war.",
+        "assets/images/thumbnails/TRAMA Una inestabilidad Still 1.jpg",
+        '<iframe src="https://player.vimeo.com/video/635702551?h=4f70e73e00" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'With: Denisse Bustos, Dama David, Pato Balado, Camila Torres, Julián Ekar, Gustavo Urroz, Ludmila Man, Sharon Cohen Goldfrid, Taiu Braver Iosovich, Sebastián Minces, Sara Jiménez Molina, Anderson Mendoza, Rafael Laureiro, Jésica Ferro, Gordon, Amigo \n Direction, production, editing: Jada Sirkin, Dama David \n Camera and color: Jada Sirkin \n Direct sound: José Ratti, Bayron Mendivelso, Dama David, James Vallejo Mejía, Gustavo Urroz, Julián Ekar. \n Sound post production: Zub sonido - Emiliano Biaiñ, Marcos Zoopi, Facundo de Vedia \n Still photo and production assistance: Malena Morandini \n Transcription: Magdalena Chattah \n Music: Joaquín Merino'
+    ],
+    [
+        'El callejón',
+        '1h 1m',
+        "Sara es una joven venezolana que vive en Vancouver y trabaja limpiando casas. Un conocido de Argentina, Max, no tiene dónde vivir y ella lo invita a pasar unos días en su cueva. También lo invita a trabajar con ella y a hacer un ayuno de jugos de una semana. Una dibujante los llama para ordenar su casa y los termina pintando. Max camina.",
+        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
+        '<iframe src="https://player.vimeo.com/video/629299340?h=16d2f4cec7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'Con: Dama David, Polilovee, Rebecca Seary, Beth Dempster, Aram Larsen, Randy, Karen Pitkethly \n Guión: Dama David, Polilovee, Rebecca Seary, Jada Sirkin \n Dirección, producción y montaje: Jada Sirkin, Dama David \n Cámara y color: Jada Sirkin \n Sonido directo: Dama David, Jada Sirkin \n Drone: Cristóbal David Dibujos: Rebecca Seary, Aram Larsen \n Post producción de sonido: Zub sonido - Emiliano Biaiñ, Marcos Zoopi, Facundo de Vedia \n Desgrabación y traducción: Clemente Felmer \n Música original: Julián Romero'
+    ],
+    [
+        'Sonidos que no existen',
+        '21min',
+        "Caro hace el amor con la luz apagada, dice que no le gusta su cuerpo. Atiende pacientes, pasea por Tinder, mira por la ventana y se imagina el infinito.",
+        "https://images2.minutemediacdn.com/image/upload/c_crop,h_674,w_1200,x_0,y_113/f_auto,q_auto,w_1100/v1554999553/shape/mentalfloss/504605-publicdomain.gif",
+        '<iframe src="https://player.vimeo.com/video/629299340?h=16d2f4cec7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'Con: Sharon Cohen Goldfrid, Gustavo Urroz, Marcos Zoppi, Mumi Rujana, Ludmila Mann, Lula Rovatti, Melina Boyadjian, Martín Sobel, Gustavo Arcuri, Valentín Piñeyro. \n Idea y producción: Sharon Cohen Goldfrid, Dama David, Jada Sirkin \n Dirección y montaje: Jada Sirkin, Dama David Cámara y color: Jada Sirkin \n Sonido directo: José Ratti, Martín Sobel, Maga Taiani, Dama David \n Guión: Sharon Cohen Goldfrid, Dama David, Jada Sirkin, Gustavo Urroz, Marcos Zoppi, Mumi Rujana, Ludmila Mann, Lula Rovatti, Melina Boyadjian, Martin Sobel, Gustavo Arcuri \n Portada y dibujos final: Álvaro Villagra Morales \n Desgrabación: Clemente Felmer Música: Ultrazoppi, Sharon Cohen Golfrid, Mumi Rujana'
+    ],
+    [
+        'Fin',
+        '1h 17min',
+        "En los primeros meses de cuarentena, Camila y Lucas ven una película. Ella se emociona mucho y él se pone duro. Pasan los días y los engranajes de la convivencia revelan hábitos y novedades. ¿Qué nos va a pasar?",
+        "https://i.pinimg.com/originals/47/7b/7c/477b7c1c999a42694b936fd191279b84.gif",
+        '<iframe src="https://player.vimeo.com/video/629299340?h=16d2f4cec7&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen style="position:absolute;top:0;left:0;width:100%;height:100%;" title="TRAMA No te tiene que doler (bajada final, 10,11,21, VBR 16-40).mp4"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>',
+        'Con: Sol Bordigoni, Jada Sirkin, Mumi Rujana, Rita Messina, Magalí Aldabe, Hernán Bustamante, Sharon Cohen Goldfrid, Jazmín Ruffo, Wem Wertheimer, Keni, Toni \n Dirección, producción, montaje: Dama David, Jada Sirkin Guión: Jada Sirkin, Dama David, Denisse Bustos, Sol Bordigoni, Rita Messina, Mumi Rujana \n Asistencia de arte: María Morín \n Cámara: Dama David, Jada Sirkin \n Sonido directo: Denisse Bustos, Camila Torres, Elsa Wirth, Dama David, Sharon Cohen Goldfrid, Wayra Inti, Facundo Martin, Julián Hercovich. \n Postproducción de sonido: Zub sonido - Emiliano Biaiñ, Marcos Zoppi, Facundo de Vedia \n Desgrabación: Magdalena Chattah \n Música: Eduardo Felenbok, Mumi Rujana, Jada Sirkin'
+    ],
+]
--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-// window.addEventListener('click', function(e){
-//     if(! this.document.getElementById('description').contains(e.target)){
-//         document.body.removeChild(document.getElementById("description"));
-//     }
-// })
-
 function renderDescription(d){
     if(document.getElementById("description")){
         document.body.removeChild(document.getElementById("description"));
@@ -23,10 +17,14 @@ function renderDescription(d){
 
     let title = document.createElement('h3');
     title.id = 'title';
-    title.innerText = descriptionsES[randomNum][0] + ' ';
+    if(EN){ title.innerText = descriptionsEN[randomNum][0] + ' '; } else {
+        title.innerText = descriptionsES[randomNum][0] + ' ';
+    }
+    
     document.getElementById("descriptionText").appendChild(title);
 
     let duracion = document.createElement('h4');
+
     duracion.innerText = descriptionsES[randomNum][1] + ' ';
     document.getElementById("descriptionText").appendChild(duracion);
 
@@ -35,7 +33,10 @@ function renderDescription(d){
     // document.getElementById("descriptionText").appendChild(timeIcon);
 
     let description = document.createElement('p');
-    description.innerText = descriptionsES[randomNum][2];
+    if(EN) { description.innerText = descriptionsEN[randomNum][2]; } else {
+        description.innerText = descriptionsES[randomNum][2];
+    }
+    
     document.getElementById("descriptionText").appendChild(description);
 
     let buttons = document.createElement('div')
@@ -43,7 +44,7 @@ function renderDescription(d){
     document.getElementById('description').appendChild(buttons);
 
     let ver = document.createElement('span');
-    ver.innerText = "Ver episodio"
+    if (EN) { ver.innerText = "View episode" } else { ver.innerText = "Ver episodio" }
     // ver.classList.add('icofont-play-alt-2');
     ver.id = 'ver';
     ver.addEventListener('click', function(){
@@ -52,7 +53,7 @@ function renderDescription(d){
     document.getElementById('buttonsContainer').appendChild(ver);
 
     let closeBtn = document.createElement('a')
-    closeBtn.innerText = 'Regresar'
+    if (EN) { closeBtn.innerText = 'Back' } else { closeBtn.innerText = 'Regresar' }
     closeBtn.id = 'closeBtn'
     buttons.appendChild(closeBtn)
     closeBtn.addEventListener('click', function(){
@@ -78,7 +79,7 @@ function renderEpisode(randomNum){
     header.id = 'header';
 
     let regresar = document.createElement('a');
-    regresar.innerText = '< Regresar';
+    if (EN) { regresar.innerText = '< Back';} else { regresar.innerText = '< Regresar'; }
     regresar.id = 'regresar';
     regresar.addEventListener('click', function(){
         document.body.removeChild(document.getElementById("episode"));
@@ -87,7 +88,9 @@ function renderEpisode(randomNum){
     document.getElementById("header").appendChild(regresar);
 
     let title = document.createElement('h2');
-    title.innerText = descriptionsES[randomNum][0];
+    if (EN) { title.innerText = descriptionsEN[randomNum][0]; } else {
+        title.innerText = descriptionsES[randomNum][0];
+    }
     title.id = 'episodeTitle'
     document.getElementById("header").appendChild(title);
 
@@ -105,12 +108,22 @@ function renderEpisode(randomNum){
     info.id = "info";
 
     let description = document.createElement('p');
-    description.innerText = descriptionsES[randomNum][2];
+
+    if (EN) { description.innerText = descriptionsEN[randomNum][2]; } else {
+        description.innerText = descriptionsES[randomNum][2];
+    }
+
+    // description.innerText = descriptionsES[randomNum][2];
     description.id = 'episodeDescription';
     document.getElementById("info").appendChild(description);
 
     let ficha = document.createElement('p');
-    ficha.innerText = descriptionsES[randomNum][5];
+
+    if (EN) { ficha.innerText = descriptionsEN[randomNum][5]; } else {
+        ficha.innerText = descriptionsES[randomNum][5];
+    }
+
+    // ficha.innerText = descriptionsES[randomNum][5];
     ficha.id = 'episodeFicha'
     document.getElementById("info").appendChild(ficha);
 
